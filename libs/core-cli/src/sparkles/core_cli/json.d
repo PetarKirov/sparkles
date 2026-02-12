3bc2fdccbd9597510a2433d60adc15809dd895ef
Petar Kirov <petar.p.kirov@gmail.com>
2026-02-12 16:22:39 +0200

    wip: copy json serde from nixos-modules/mcl

---
+++ libs/core-cli/src/sparkles/core_cli/json.d
@@ -0,0 +1,409 @@
+module sparkles.core_cli.json;
+
+import std.traits: isNumeric, isArray, isSomeChar, EnumMembers, ForeachType, isBoolean, isAssociativeArray;
+import std.json: parseJSON, JSONValue, JSONOptions, JSONType;
+import std.conv: to;
+import std.string: strip;
+import std.range: front;
+import std.algorithm: map;
+import std.array: join, array, replace, split;
+import std.datetime: SysTime;
+import std.sumtype: SumType, isSumType;
+import std.typecons: Ternary, Nullable;
+import core.stdc.string: strlen;
+
+import sparkles.core_cli.string: enumToString, enumFromString, StringRepresentation;
+
+bool tryDeserializeJson(T)(in JSONValue value, out T result)
+{
+    try {
+        result = value.fromJSON!T;
+        return true;
+    } catch (Exception e) {
+        return false;
+    }
+}
+
+T fromJSON(T)(in JSONValue value) {
+    if (value.isNull) {
+        return T.init;
+    }
+    static if (is(T == JSONValue)) {
+        return value;
+    }
+    else static if (is(T == enum)) {
+        return value.get!string.enumFromString!T;
+    }
+    else static if (is(T == bool) || is(T == string) || isSomeChar!T || isNumeric!T) {
+        return value.get!T;
+    }
+    else static if (isSumType!T) {
+        static foreach (SumTypeVariant; T.Types)
+        {{
+            SumTypeVariant result;
+            if (tryDeserializeJson!SumTypeVariant(value, result)) {
+                return T(result);
+            }
+        }}
+
+        throw new Exception("Failed to deserialize JSON value");
+    }
+    else static if (isArray!T) {
+        static if ( isBoolean!(ForeachType!T)) {
+            if (value.type == JSONType.string && isBoolean!(ForeachType!T)) {
+                return value.str.map!(a => a == '1').array;
+            }
+        }
+
+        if (value.type != JSONType.array) {
+            return [value.fromJSON!(ForeachType!T)];
+        }
+
+        return value.array.map!(a => a.fromJSON!(ForeachType!T)).array;
+    }
+    else static if (is(T == SysTime)) {
+        return SysTime.fromISOExtString(value.toString(JSONOptions.doNotEscapeSlashes).strip("\""));
+    }
+    else static if (is(T : Nullable!U, U)) {
+        if (value.isNull) {
+            return T.init;
+        }
+        return Nullable!U(value.fromJSON!U);
+    }
+    else static if (is(T == struct)) {
+        T result;
+        static foreach (idx, field; T.tupleof) {
+            if ((__traits(identifier, field).replace("_", "") in value.object) && !value[__traits(identifier, field).replace("_", "")].isNull) {
+                result.tupleof[idx] = value[__traits(identifier, field).replace("_", "")].fromJSON!(typeof(field));
+            }
+        }
+        return result;
+    }
+    else static if (is(T == V[K], V, K)) {
+        V[K] result;
+        foreach (key, val; value.object) {
+            static if (is(K == enum)) {
+                result[key.enumFromString!K] = val.fromJSON!V;
+            } else {
+                static assert(is(K == string), "Associated arrays can only be indexed by strings (because JSON)");
+                result[key] = val.fromJSON!V;
+            }
+        }
+        return result;
+    }
+    else {
+        static assert(false, "Unsupported type: `", T,  "` ", isSumType!T);
+    }
+}
+
+@("fromJSON.AA")
+unittest {
+    {
+        struct Node {
+            string name;
+            ulong size;
+            Node[string] children;
+        }
+
+        auto json = parseJSON(`{
+            "name": "root",
+            "size": 42,
+            "children": {
+                "child1": {
+                    "name": "child1",
+                    "size": 1,
+                    "children": {}
+                },
+                "child2": {
+                    "name": "child2",
+                    "size": 2,
+                    "children": {
+                        "child3": {
+                            "name": "child3",
+                            "size": 3,
+                            "children": {}
+                        }
+                    }
+                }
+            }
+        }`);
+
+        assert(json.fromJSON!Node == Node("root", 42, [
+            "child1": Node("child1", 1, null),
+            "child2": Node("child2", 2, [
+                "child3": Node("child3", 3, null)
+            ])
+        ]));
+    }
+
+    {
+        alias Type = SumType!(
+            int,
+            bool[string]
+        );
+
+        auto json = parseJSON(`{
+            "1": 1,
+            "2": { "a": true }
+        }`);
+
+        assert(json.fromJSON!(Type[string]) == [
+            "1": Type(1),
+            "2": Type([ "a": true ])
+        ]);
+    }
+
+    {
+        alias Type = SumType!(
+            int,
+            bool[string]
+        );
+
+        auto json = parseJSON(`[1, { "a": true } ]`);
+
+        assert(json.fromJSON!(Type[]) == [
+            Type(1),
+            Type([ "a": true ])
+        ]);
+    }
+
+    {
+        auto json = parseJSON(`{
+            "1": "a",
+            "2": 3,
+            "3": [ 1, { "x": true } ]
+        }`);
+
+        // alias Type = SumType!(
+        //     string,
+        //     int,
+        //     SumType!(
+        //         int,
+        //         bool[string]
+        //     )
+        // )[string];
+
+        alias InnerType = SumType!(
+            int,
+            bool[string]
+        );
+
+        alias OuterType = SumType!(
+            string,
+            int,
+            InnerType[]
+        );
+
+        alias Type = OuterType[string];
+
+        Type expectedValue = [
+            "1": OuterType("a"),
+            "2": OuterType(3),
+            "3": OuterType([ InnerType(1), InnerType([ "x": true ]) ])
+        ];
+
+        assert(json.fromJSON!Type == expectedValue);
+    }
+
+    {
+        enum Kind
+        {
+            @StringRepresentation("first-kind") first,
+            second
+        }
+
+        auto json = parseJSON(`{
+            "first-kind": 1,
+            "second": 2
+        }`);
+
+        assert(json.fromJSON!(int[Kind]) == [
+            Kind.first: 1,
+            Kind.second: 2
+        ]);
+    }
+}
+
+@("fromJSON.SumType")
+unittest {
+    alias NumOrString = SumType!(int, string);
+    assert(42.JSONValue.fromJSON!NumOrString == NumOrString(42));
+    assert("test".JSONValue.fromJSON!NumOrString == NumOrString("test"));
+}
+
+@("fromJSON.Nullable")
+unittest {
+    auto jsonNull = JSONValue(null);
+    auto jsonInt = JSONValue(42);
+
+    auto nullValue = jsonNull.fromJSON!(Nullable!int);
+    auto intValue = jsonInt.fromJSON!(Nullable!int);
+
+    assert(nullValue.isNull);
+    assert(!intValue.isNull);
+    assert(intValue.get == 42);
+}
+
+JSONValue toJSON(T)(in T value, bool simplify = false)
+{
+    static if (is(T == enum))
+    {
+        return JSONValue(value.enumToString);
+    }
+    else static if (is(T == Ternary))
+    {
+        if (value == Ternary.unknown)
+            return JSONValue(null);
+        else if (value == Ternary.yes)
+            return JSONValue(true);
+        else
+            return JSONValue(false);
+    }
+    else static if (is(T == bool) || is(T == string) || isSomeChar!T || isNumeric!T)
+        return JSONValue(value);
+    else static if ((isArray!T && isSomeChar!(ForeachType!T)) ) {
+        return JSONValue(value.idup[0..(strlen(value.ptr)-1)]);
+    }
+    else static if (isArray!T)
+    {
+        if (simplify && value.length == 1)
+            return value.front.toJSON(simplify);
+        else if (simplify  && isBoolean!(ForeachType!T) ) {
+            static if (isBoolean!(ForeachType!T)) {
+                return JSONValue((value.map!(a => a ? '1' : '0').array).to!string);
+            }
+            else {assert(0);}
+        }
+        else {
+            JSONValue[] result;
+            foreach (elem; value)
+                result ~= elem.toJSON(simplify);
+            return JSONValue(result);
+        }
+    }
+    else static if (is(T == SysTime)) {
+        return JSONValue(value.toISOExtString());
+    }
+    else static if (is(T : Nullable!U, U)) {
+        if (value.isNull)
+            return JSONValue(null);
+        else
+            return value.get.toJSON!U(simplify);
+    }
+    else static if (is(T == struct))
+    {
+        JSONValue[string] result;
+        auto name = "";
+        static foreach (idx, field; T.tupleof)
+        {
+            name = __traits(identifier, field).strip("_");
+            result[name] = value.tupleof[idx].toJSON(simplify);
+        }
+        return JSONValue(result);
+    }
+    else static if (is(T == K[V], K, V))
+    {
+        JSONValue[string] result;
+        foreach (key, field; value)
+        {
+            result[key] = field.toJSON(simplify);
+        }
+        return JSONValue(result);
+    }
+    else
+        static assert(false, "Unsupported type: `" ~ __traits(identifier, T) ~ "`");
+}
+
+version(unittest)
+{
+    enum TestEnum
+    {
+        @StringRepresentation("supercalifragilisticexpialidocious")
+        a,
+        b,
+        c
+    }
+    struct TestStruct
+    {
+        int a;
+        string b;
+        bool c;
+    }
+    struct TestStruct2
+    {
+        int a;
+        TestStruct b;
+    }
+    struct TestStruct3
+    {
+        int a;
+        TestStruct2 b;
+    }
+}
+
+@("toJSON")
+unittest
+{
+    assert(1.toJSON == JSONValue(1));
+    assert(true.toJSON == JSONValue(true));
+    assert("test".toJSON == JSONValue("test"));
+    assert([1, 2, 3].toJSON == JSONValue([1, 2, 3]));
+    assert(["a", "b", "c"].toJSON == JSONValue(["a", "b", "c"]));
+    assert([TestEnum.a, TestEnum.b, TestEnum.c].toJSON == JSONValue(["supercalifragilisticexpialidocious", "b", "c"]));
+    TestStruct testStruct = { 1, "test", true };
+    assert(testStruct.toJSON == JSONValue(["a": JSONValue(1), "b": JSONValue("test"), "c": JSONValue(true)]));
+    TestStruct2 testStruct2 = { 1, testStruct };
+    assert(testStruct2.toJSON == JSONValue(["a": JSONValue(1), "b": JSONValue(["a": JSONValue(1), "b": JSONValue("test"), "c": JSONValue(true)])]));
+    TestStruct3 testStruct3 = { 1, testStruct2 };
+    assert(testStruct3.toJSON == JSONValue(["a": JSONValue(1), "b": JSONValue(["a": JSONValue(1), "b": JSONValue(["a": JSONValue(1), "b": JSONValue("test"), "c": JSONValue(true)])])]));
+
+    Nullable!int nullValue = Nullable!int.init;
+    Nullable!int intValue = Nullable!int(42);
+    assert(nullValue.toJSON == JSONValue(null));
+    assert(intValue.toJSON == JSONValue(42));
+}
+
+
+T tryGet(T)(lazy T value, string errorMsg, string file = __FILE__, size_t line = __LINE__)
+{
+    try
+    {
+        return value;
+    }
+    catch (Exception e)
+    {
+        throw new Exception(errorMsg, file, line, e);
+    }
+}
+
+T tryDeserializeFromJsonFile(T)(string path)
+{
+    import std.file : readText;
+    import std.format : format;
+    import std.json : parseJSON;
+    import std.string : strip;
+
+    import mcl.utils.tui : bold;
+
+    auto txt = path
+        .readText()
+        .strip()
+        .tryGet("Error reading file: '%s'".format(path.bold));
+
+    auto json = txt
+        .parseJSON()
+        .tryGet("Error parsing JSON. File contents: '%s'".format(txt.bold));
+
+    return json
+        .fromJSON!T()
+        .tryGet("Error deserializing %s. JSON: \n%s".format(T.stringof.bold, json.toPrettyString().bold));
+}
+
+void writeJsonFile(T)(in T value, const(char)[] path)
+{
+    import std.path : dirName;
+    import std.file : mkdirRecurse, writeFile = write;
+    auto json = value.toJSON.toPrettyString(JSONOptions.doNotEscapeSlashes);
+    mkdirRecurse(path.dirName);
+    writeFile(path, json);
+}
---
+++ libs/core-cli/src/sparkles/core_cli/string.d
@@ -0,0 +1,350 @@
+module sparkles.core_cli.string;
+
+import std.conv : to;
+import std.exception : assertThrown;
+
+string lowerCaseFirst(string r)
+{
+    import std.range : save, popFront, chain, front, only;
+    import std.uni : toLower;
+
+    string rest = r.save;
+    rest.popFront();
+    return chain(r.front.toLower.only, rest).to!string;
+}
+
+string camelCaseToCapitalCase(string camelCase)
+{
+    import std.algorithm : splitWhen, map, joiner;
+    import std.conv : to;
+    import std.uni : isLower, isUpper, asUpperCase;
+
+    return camelCase
+        .splitWhen!((a, b) => isLower(a) && isUpper(b))
+        .map!asUpperCase
+        .joiner("_")
+        .to!string;
+}
+
+@("camelCaseToCapitalCase")
+unittest
+{
+    assert(camelCaseToCapitalCase("camelCase") == "CAMEL_CASE");
+
+    assert(camelCaseToCapitalCase("") == "");
+    assert(camelCaseToCapitalCase("_") == "_");
+    assert(camelCaseToCapitalCase("a") == "A");
+    assert(camelCaseToCapitalCase("ab") == "AB");
+    assert(camelCaseToCapitalCase("aB") == "A_B");
+    assert(camelCaseToCapitalCase("aBc") == "A_BC");
+    assert(camelCaseToCapitalCase("aBC") == "A_BC");
+    assert(camelCaseToCapitalCase("aBCD") == "A_BCD");
+    assert(camelCaseToCapitalCase("aBcD") == "A_BC_D");
+
+    assert(camelCaseToCapitalCase("rpcUrl") == "RPC_URL");
+    assert(camelCaseToCapitalCase("parsedJSON") == "PARSED_JSON");
+    assert(camelCaseToCapitalCase("fromXmlToJson") == "FROM_XML_TO_JSON");
+    assert(camelCaseToCapitalCase("fromXML2JSON") == "FROM_XML2JSON");
+}
+
+string kebabCaseToCamelCase(string kebabCase)
+{
+    import std.algorithm : map;
+    import std.string : capitalize;
+    import std.array : join, split;
+
+    return kebabCase
+        .split("-")
+        .map!capitalize
+        .join
+        .to!string
+        .lowerCaseFirst;
+}
+
+@("kebabCaseToCamelCase")
+unittest
+{
+    assert(kebabCaseToCamelCase("kebab-case") == "kebabCase");
+    assert(kebabCaseToCamelCase("kebab-case-") == "kebabCase");
+    assert(kebabCaseToCamelCase("kebab-case--") == "kebabCase");
+    assert(kebabCaseToCamelCase("kebab-case--a") == "kebabCaseA");
+    assert(kebabCaseToCamelCase("kebab-case--a-") == "kebabCaseA");
+
+    assert(kebabCaseToCamelCase(
+            "once-upon-a-midnight-dreary-while-i-pondered-weak-and-weary" ~
+            "-over-many-a-quaint-and-curious-volume-of-forgotten-lore" ~
+            "-while-i-nodded-nearly-napping-suddenly-there-came-a-tapping" ~
+            "-as-of-someone-gently-rapping-rapping-at-my-chamber-door" ~
+            "-tis-some-visitor-i-muttered-tapping-at-my-chamber-door" ~
+            "-only-this-and-nothing-more") == "onceUponAMidnightDrearyWhileIPonderedWeakAndWeary" ~
+            "OverManyAQuaintAndCuriousVolumeOfForgottenLore" ~ "WhileINoddedNearlyNappingSuddenlyThereCameATapping" ~
+            "AsOfSomeoneGentlyRappingRappingAtMyChamberDoor" ~
+            "TisSomeVisitorIMutteredTappingAtMyChamberDoor" ~
+            "OnlyThisAndNothingMore");
+
+}
+
+struct StringRepresentation
+{
+    string repr;
+}
+
+string enumToString(E)(in E value) if (is(E == enum))
+{
+    import std.traits : EnumMembers, hasUDA, getUDAs;
+
+    final switch (value)
+    {
+        static foreach (enumMember; EnumMembers!E)
+        {
+            case enumMember:
+            {
+                static if (!hasUDA!(enumMember, StringRepresentation))
+                {
+                    debug pragma(msg,
+                        "Enum memer doesn't have StringRepresentation: ",
+                        enumMember
+                    );
+                    return enumMember.to!string;
+                }
+                else
+                    return getUDAs!(enumMember, StringRepresentation)[0].repr;
+            }
+        }
+    }
+}
+
+@("enumToString")
+unittest
+{
+    enum TestEnum
+    {
+        a1,
+        b2,
+        c3
+    }
+
+    assert(enumToString(TestEnum.a1) == "a1");
+    assert(enumToString(TestEnum.b2) == "b2");
+    assert(enumToString(TestEnum.c3) == "c3");
+
+    enum TestEnumWithRepr
+    {
+        @StringRepresentation("field1") a,
+        @StringRepresentation("field_2") b,
+        @StringRepresentation("field-3") c
+    }
+
+    assert(enumToString(TestEnumWithRepr.a) == "field1");
+    assert(enumToString(TestEnumWithRepr.b) == "field_2");
+    assert(enumToString(TestEnumWithRepr.c) == "field-3");
+}
+
+E enumFromString(E)(in string value) if (is(E == enum))
+{
+    import std.traits : EnumMembers;
+
+    switch (value) {
+        static foreach (variant; EnumMembers!E)
+            case variant.enumToString: return variant;
+        default:
+            assert(0, "Expected JSON string, actual: " ~ value ~ " while deserializing type " ~ E.stringof);
+    }
+}
+
+@("enumFromString")
+unittest
+{
+    enum TestEnum
+    {
+        a1,
+        b2,
+        c3
+    }
+
+    assert(enumFromString!TestEnum("a1") == TestEnum.a1);
+    assert(enumFromString!TestEnum("b2") == TestEnum.b2);
+    assert(enumFromString!TestEnum("c3") == TestEnum.c3);
+
+    enum TestEnumWithRepr
+    {
+        @StringRepresentation("field1") a,
+        @StringRepresentation("field_2") b,
+        @StringRepresentation("field-3") c
+    }
+
+    assert(enumFromString!TestEnumWithRepr("field1") == TestEnumWithRepr.a);
+    assert(enumFromString!TestEnumWithRepr("field_2") == TestEnumWithRepr.b);
+    assert(enumFromString!TestEnumWithRepr("field-3") == TestEnumWithRepr.c);
+
+    assertThrown!Error(enumFromString!TestEnum("invalid"));
+}
+
+enum size_t getMaxEnumMemberNameLength(E) = ()
+{
+    import std.traits : EnumMembers;
+
+    size_t max = 0;
+    foreach (member; [EnumMembers!E])
+    {
+        const name = member.enumToString();
+        max = name.length > max ? name.length : max;
+    }
+
+    return max;
+}();
+
+@("getMaxEnumMemberNameLength")
+unittest
+{
+    enum EnumLen
+    {
+        @StringRepresentation("a1") a,
+        @StringRepresentation("b12") b,
+        @StringRepresentation("c123") c
+    }
+
+    static assert(getMaxEnumMemberNameLength!EnumLen == 4);
+}
+
+struct MaxWidth
+{
+    ushort value;
+}
+
+void writeRecordAsTable(bool ansiColors = true, T, Writer)(in T obj, auto ref Writer w)
+{
+    import std.format : formattedWrite;
+    import std.traits : hasUDA, getUDAs;
+
+    const gray = ansiColors ? "\x1b[90m" : "";
+    const bold = ansiColors ? "\x1b[1m" : "";
+    const normal = ansiColors ? "\x1b[0m" : "";
+
+    w.formattedWrite("│");
+    static foreach (idx, field; T.tupleof)
+    {{
+        // If the field is an enum, get the maximum length of the enum member names
+        static if (is(typeof(field) == enum))
+            const width = getMaxEnumMemberNameLength!(typeof(field));
+
+        // If the field is a bool, set the width to "false".length
+        else static if (is(typeof(field) : bool))
+            const width = 5;
+
+        // If the field has a UDA MaxWidth, set the width to the value of the UDA
+        else static if (hasUDA!(field, MaxWidth))
+            const width = getUDAs!(field, MaxWidth)[0].value;
+
+        else
+            const width = 0;
+
+        w.formattedWrite(
+            " %s%s%s: %s%*-s%s │",
+            gray, __traits(identifier, field), normal,
+            bold, width, obj.tupleof[idx], normal
+        );
+    }}
+    w.formattedWrite("\n");
+}
+
+@("writeRecordAsTable")
+unittest
+{
+    import std.array : appender;
+
+    struct TestStruct
+    {
+        int num;
+        @MaxWidth(4) int otherNum;
+        bool bool1;
+        bool bool2;
+        @MaxWidth(10) string someString;
+    }
+
+    const t = TestStruct(1, 20, true, false, "test");
+    auto result = appender!string;
+    t.writeRecordAsTable!false(result);
+    assert(result.data == "│ num: 1 │ otherNum: 20   │ bool1: true  │ bool2: false │ someString: test       │\n");
+}
+
+import std.string : chomp, stripLeft;
+
+/**
+ * Appends a path segment to a base URL string.
+ *
+ * This function ensures that exactly one forward slash separator exists between
+ * the `baseUrl` and the `path`. It handles cases where either string may or
+ * may not already contain a slash, preventing double slashes (e.g., `//`).
+ *
+ * Params:
+ * baseUrl = The starting URL (e.g., "https://api.example.com").
+ * path    = The path segment to append (e.g., "/v1/users").
+ *
+ * Returns:
+ * A new string containing the combined URL.
+ *
+ * Example:
+ * ---
+ * string url = appendUrlPath("https://dlang.org", "/spec");
+ * assert(url == "https://dlang.org/spec");
+ *
+ * // Handles redundant slashes gracefully
+ * assert(appendUrlPath("http://site.com/", "/api") == "http://site.com/api");
+ * ---
+ */
+string appendUrlPath(string baseUrl, string path)
+{
+    if (baseUrl.length == 0)
+    {
+        return path;
+    }
+    if (path.length == 0)
+    {
+        return baseUrl;
+    }
+
+    return baseUrl.chomp("/") ~ "/" ~ path.stripLeft("/");
+}
+
+@("appendUrlPath.standardJoining")
+unittest
+{
+    assert(appendUrlPath("https://dlang.org", "spec") == "https://dlang.org/spec");
+    assert(appendUrlPath("localhost:8080", "api/v1") == "localhost:8080/api/v1");
+}
+
+@("appendUrlPath.slashNormalization")
+unittest
+{
+    // Test trailing slash on base
+    assert(appendUrlPath("http://example.com/", "path") == "http://example.com/path");
+
+    // Test leading slash on path
+    assert(appendUrlPath("http://example.com", "/path") == "http://example.com/path");
+
+    // Test both slashes present
+    assert(appendUrlPath("http://example.com/", "/path") == "http://example.com/path");
+}
+
+@("appendUrlPath.emptyInputs")
+unittest
+{
+    assert(appendUrlPath("", "only-path") == "only-path");
+    assert(appendUrlPath("only-base", "") == "only-base");
+    assert(appendUrlPath("", "") == "");
+}
+
+@("appendUrlPath.multipleLeadingSlashes")
+unittest
+{
+    // Ensure it cleans up accidental triple slashes in the path segment
+    assert(appendUrlPath("http://api.com", "///v1/user") == "http://api.com/v1/user");
+}
+
+@("appendUrlPath.rootPath")
+unittest
+{
+    // Ensure appending a single slash results in a valid trailing slash URL
+    assert(appendUrlPath("http://site.com", "/") == "http://site.com/");
+}
