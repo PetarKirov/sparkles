28270b75cc06e8d53d794e0cb3a8204ee01cdffb
reo101 <pavel.atanasov2001@gmail.com>
2026-03-18 15:30:30 +0200

    perf(math): speed up VGA multivector compile-time lookups
    - Pack `Multivector` support masks into `size_t` words while
      keeping the `Multivector!(T, N, masks)` API
    - Add dense `mask -> coeff index` tables and a same-support
      `opEquals()` fast path to avoid repeated `indexOfMask()`
      scans and `coefficient!mask` instantiations
    - Gate 12D basis/proxy coverage behind
      `version(VGAHeavyCompileTests)` and add `library-heavy`
      and `unittest-heavy` configs
    - Enable `ldc` `-ftime-trace` in `libs/math/dub.sdl`
      and refresh `libs/math/examples/vga.d`

--- libs/math/examples/vga.d
+++ libs/math/examples/vga.d
@@ -6,7 +6,7 @@ dependency "sparkles:math" version="*"
 targetPath "build"
 +/
 
-import std.stdio : writefln;
+import std.stdio : writeln, writefln;
 
 import sparkles.core_cli.prettyprint : prettyPrint;
 import sparkles.math.vga;
@@ -14,4 +14,13 @@ import sparkles.math.vga;
 void main()
 {
     gradeBladeMasks!(4, 1).writefln!"[%(0b%04b, %)]";
+
+    alias Vec3 = GAVector!(double, 3);
+    alias E3 = Basis!(double, 3);
+
+    auto v = Vec3(1, 2, 3);
+    // v.prettyPrint().writeln;
+    // typeof(v.coeffs).stringof.writeln;
+
+    choose!(3, 3).prettyPrint.writeln;
 }
