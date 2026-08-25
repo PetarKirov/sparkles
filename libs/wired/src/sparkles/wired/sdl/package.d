978710e178a104cfe0ccf3c1e89b70cce6ae6b96
Petar Kirov <petar.p.kirov@gmail.com>
2026-08-26 00:44:55 +0300

    feat(wired.sdl): add atomic file helpers with stage-specific errors

--- libs/wired/src/sparkles/wired/sdl/package.d
+++ libs/wired/src/sparkles/wired/sdl/package.d
@@ -5,6 +5,7 @@ public import sparkles.wired.sdl.codec;
 public import sparkles.wired.sdl.config;
 public import sparkles.wired.sdl.document;
 public import sparkles.wired.sdl.error;
+public import sparkles.wired.sdl.files;
 public import sparkles.wired.sdl.lexer;
 public import sparkles.wired.sdl.reader;
 public import sparkles.wired.sdl.schema_annotations;
